37855d93f04e0d36de7ec1f4341a22ba19cf202d
Irfin Falah <irfin0falah@gmail.com>
2025-09-15 06:35:09 +0700

    delete product to cascade

--- src/app/(loby)/loading.tsx
+++ src/app/(loby)/loading.tsx
@@ -5,7 +5,7 @@ export default function LoadingRoot() {
   return (
     <CardLoby
       title="Niaga Order"
-      subTitle={`Create Order by {session.user.name}`}
+      subTitle={`Made by Mr.fal`}
       description="Enter your email below to login to your account"
     >
       <FormOrderSkeleton />
